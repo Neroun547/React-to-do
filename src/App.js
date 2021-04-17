a21cb8b00ce9b09735fcceb0bb33100e25bd4521
Gosha <george.kovalenko.2005@gmail.com>
2021-04-17 19:09:41 +0300

    fixed ...

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Todo from './Components/Todo'
+import './main.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="logo-project">To do app React JS</h2>
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
